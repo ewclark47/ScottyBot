3cc5f1b0a9b38e0cbd5ab90fea3e4bc98948f89a
ewclark47 <93678940+ewclark47@users.noreply.github.com>
2022-12-04 17:12:40 -0500

    docs: added inline documentation and script summary at the top. feat: verified adjusted call to LUIS responses with including the recognized entities. chore: database functionality

--- mainbot.py
+++ mainbot.py
@@ -1,3 +1,19 @@
+"""
+author: Elliott Clark
+This script initializes a Slack Bot and a Mircrosoft Azure LUIS project. The Bot
+then performs one of various functions based on user input whenever a user in the
+Slack channel types @ScottyBot with a message. This script pulls out the pertinent
+message and user information to then pass on to the LUIS module and the database 
+module. Once the database and LUIS modules have performed their functions then
+the Bot will respond based on what function was completed.
+The functions that ScottyBot currently performs are:
+AddCourse - add a course to a users schedule based on courseNumber
+DropCourse - removes a course from a users schedule based on courseNumber
+FindCourse - searches available courses and tries to match what the user is requesting
+ViewSchedule - displays the users current schedule
+"""
+
+# imports
 from os import environ
 from dotenv import load_dotenv
 from slack_bolt import App
@@ -5,60 +21,67 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler
 from LUIS_functions import LUIS
 import database_functions
 
+# set up the environment 
 load_dotenv()
 SLACK_BOT_TOKEN = environ['SLACK_BOT_TOKEN']
 SLACK_APP_TOKEN = environ['SLACK_APP_TOKEN']
-# get secrets
 clu_endpoint = environ["AZURE_CONVERSATIONS_ENDPOINT"]
 clu_key = environ["AZURE_CONVERSATIONS_KEY"]
 project_name = environ["AZURE_CONVERSATIONS_PROJECT_NAME"]
 deployment_name = environ["AZURE_CONVERSATIONS_DEPLOYMENT_NAME"]
 
+# intialize the bolt app for ScottyBot and the LUIS functionality
 app = App(token = SLACK_BOT_TOKEN)
 luis = LUIS(clu_endpoint, clu_key, project_name, deployment_name)
 
-
+# LUIS response and database functionality based on LUIS determined intent
 def switch(action, categories, values, username):
+	luisResponse="Sorry, I didn't quite get that"
 	if action == "AddCourse":
 		# then go through entity categorys to see if there is a course number, if not request it
 		for i in range(len(categories)):
 			if categories[i]=='CourseNumber':
-				database_functions.addCourse(username, values[i])
-				res = luis.addCourse(values[i])
+				#database_functions.addCourse(username, values[i]) # database functions not fully ready
+				luisResponse = luis.addCourse(values[i])
+			else:
+				luisResponse = "Please try again, I didn't quite get that course number"
 	elif action == "DropCourse":
 		# then go through entity categorys to see if there is a course number, if not request it
 		for i in range(len(categories)):
 			if categories[i]=='CourseNumber':
-				database_functions.dropCourse(username, values[i])
-				res = luis.addCourse(values[i])
-		res = luis.dropCourse(12345)
+				#database_functions.dropCourse(username, values[i]) # database functions not fully ready
+				luisResponse = luis.dropCourse(values[i])
+			else:
+				luisResponse = "Please try again, I didn't quite get that course number"
 	elif action == "FindCourse":
 		# go through entities and find a topic or something to pass on to LUIS
-		res = luis.findCourse("Just something to test here")
+		luisResponse = luis.findCourse("Just something to test here")
 	elif action == "ViewSchedule":
-		scheduleString = database_functions.viewSchedule(username)
-		res = luis.viewSchedule(scheduleString)
-	return res
+		scheduleString = "This will be replaced by info from the database"
+		#scheduleString = database_functions.viewSchedule(username) # database functions not fully ready
+		luisResponse = luis.viewSchedule(scheduleString)
+	return luisResponse
 
 @app.event("app_mention")
 def mention_handler(body, say):
 	event = body['event'] # pulls the part of the body dictionary that contains the user message
 	text = event['text'] 
 	user = "<@" + event['user'] + ">"
-	username=user[1:]
+	username=user[1:] # the users name without the @ symbol for use in the database
 	message = text[15:] # this is just the user input message without the bot name
 	# the above is the full uterance
-	print(event)
+	#print(event) 
 	print(message)
 	
-
-	result = luis.getLuisResults(message)
+	# store the intent that LUIS determined from user input
+	luisPrediction = luis.getLuisResults(message)
 	luis.displayResults(message)
 
-	action = result["result"]["prediction"]["topIntent"]
+	# store all LUIS discovered entity categories and their values i.e. category: CourseNumber, value: 95729
+	action = luisPrediction["result"]["prediction"]["topIntent"]
 	entity_cats = []
 	entity_vals = []
-	for entity in result["result"]["prediction"]["entities"]:
+	for entity in luisPrediction["result"]["prediction"]["entities"]:
 		entity_cats.append(entity["category"])
 		entity_vals.append(entity["text"])
         

aad7cb59a0cff44a5e9b270f64cef3070fa33520
ewclark47 <93678940+ewclark47@users.noreply.github.com>
2022-12-11 18:26:32 -0500

    feat: created basic structure for the webhook api. chore: find a way to pull the users name that is messaging the bot for use with the database

--- dialogflow.py
+++ dialogflow.py
@@ -1,12 +1,55 @@
-class DialogFlow:
-    def __init__(self):
-        import dialogflow
-        from google.api_core.exceptions import InvalidArgument
+from flask import Flask, request
+from os import environ
+from dotenv import load_dotenv
+import database_functions
+import openai
+from gpt3 import gpt3
 
-        DIALOGFLOW_PROJECT_ID='INSERT ID HERE'
-        DIALOGFLOW_LANGUAGE_CODE= 'en-US'
-        GOOGLE_APPLICATION_CREDENTIALS = 'CREDENTIALS JSON HERE'
-        SESSION_ID = 'current-user-id here'
+load_dotenv()
 
-        session_client = dialogflow.SessionsClient()
-        session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+openai.api_key = environ["OPENAI_API_KEY"]
+
+gpt3 = gpt3(openai.api_key)
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    print("Received request from df")
+    req = request.get_json(silent = True, force=True)
+    fulfillmentText = ''
+    df_query = req.get('queryResult') 
+    df_intent = df_query.get('intent')
+    action = ''
+    if "displayName" in df_intent.keys():
+        action = df_intent['displayName']
+    message = df_query.get('queryText')
+    params = df_query.get('parameters') # this is a dictionary
+    print("retrieved necessary info, starting switch statement")
+    fulfillmentText = switch(action, message)
+    print("fulfillment text: " + fulfillmentText)
+
+    return {
+        "fulfillmentText": fulfillmentText,
+        "source": "webhookdata"
+    }
+    
+
+def switch(action, message):
+    print("started switch statment. responding based on action: " + action)
+    df_response ="Sorry, I didn't quite get that"
+    if action == "AddCourse":
+        df_response = "Dialogflow says adding course stuff here"
+
+    elif action == "DropCourse":
+        df_response = "Dialogflow says dropping course stuff here"
+    elif action == "FindCourse":
+        df_response = "Dialogflow says finding course stuff here"
+    elif action == "ViewSchedule":
+        df_response = "Dialogflow says viewing schedule stuff here"
+    else: 
+        df_response = gpt3.gpt_response(message)
+    return df_response
+
+if __name__ == '__main__':
+    app.run()
--- gpt3.py
+++ gpt3.py
@@ -5,13 +5,13 @@ class gpt3:
         self.api_key = api_key
 
     def gpt_response(self, input):
-            response = openai.Completion.create(
-                    engine="text-davinci-002",
-                    prompt=input,
-                    temperature=0.7,
-                    max_tokens=1000,
-                    top_p=1.0,
-                    frequency_penalty=0.0,
-                    presence_penalty=0.0
-            )
-            return response.choices[0].text
+        response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=input,
+                temperature=0.7,
+                max_tokens=1000,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+        )
+        return response.choices[0].text
